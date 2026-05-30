57f8a0115427d2b0f1b0828886a0dd41eb10b65e
zhuuvip <hgeming2009@gmail.com>
2026-05-30 10:46:50 +0700

    add PWA support

---
+++ artifacts/zhuu-vip/public/sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'zhuuvip-v1';
+const STATIC_ASSETS = ['/', '/index.html'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
