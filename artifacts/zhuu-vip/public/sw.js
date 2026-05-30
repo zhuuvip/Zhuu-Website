d17aa072d1d6884654f3b363bd905e6a4490de72
zhuuvip <hgeming2009@gmail.com>
2026-05-30 15:43:21 +0700

    fix service worker cache

--- artifacts/zhuu-vip/public/sw.js
+++ artifacts/zhuu-vip/public/sw.js
@@ -1,14 +1,18 @@
-const CACHE_NAME = 'zhuuvip-v1';
-const STATIC_ASSETS = ['/', '/index.html'];
+const CACHE_NAME = 'zhuuvip-v2';
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
+    caches.keys().then(keys => 
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
   );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
